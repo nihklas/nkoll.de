4c379fe30ed3938c84442ab304b9a7fa90381802
Niklas Koll <niklas.koll@dynabase.de>
2026-05-01 10:45:01 +0200

    Add forgejo and gitlab icons and links

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,14 @@
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconGitlab from "@/assets/icons/IconGitlab.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconForgejo from "@/assets/icons/IconForgejo.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -18,26 +20,32 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
+    name: "Forgejo",
+    href: "https://git.nkoll.de",
+    linkTitle: `${SITE.title} on Forgejo`,
+    icon: IconForgejo,
+  },
+  {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/nihklas",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
+    name: "Gitlab",
+    href: "https://gitlab.com/nihklas",
+    linkTitle: `${SITE.title} on Gitlab`,
+    icon: IconGitlab,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/niklas-koll/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:niklas@nkoll.de",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
