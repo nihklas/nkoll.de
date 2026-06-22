348083e27dfce75e3e1862315078cc8352a5b1c6
Niklas Koll <niklas@nkoll.de>
2026-06-22 23:04:39 +0200

    Add instagram link

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,7 @@ import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconInstagram from "@/assets/icons/IconInstagram.svg";
 import IconForgejo from "@/assets/icons/IconForgejo.svg";
 import { SITE } from "@/config";
 
@@ -37,6 +38,12 @@ export const SOCIALS: Social[] = [
     href: "https://gitlab.com/nihklas",
     linkTitle: `${SITE.title} on Gitlab`,
     icon: IconGitlab,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/nihklas_/",
+    linkTitle: `${SITE.title} on Instagram`,
+    icon: IconInstagram,
   },
   {
     name: "LinkedIn",
